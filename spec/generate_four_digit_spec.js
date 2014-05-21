69470a8fb2c8affc6aeb9dd5ac4d76bed590d545
zhangyufei <momosuyu@gmail.com>
2014-05-21 16:37:38 +0800

    four-digit is andomly generated and not repeated

---
+++ spec/generate_four_digit_spec.js
@@ -0,0 +1,44 @@
+describe("Number", function () {
+
+
+    beforeEach(function () {
+
+    });
+
+    it("should be number", function () {
+        var num = Number.create();
+
+        expect(isNaN(num)).toBe(false);
+    });
+
+    it("should be four-digit", function () {
+        var num = Number.create();
+
+        expect(num >= 1000 && num <= 9999).toBe(true)
+    })
+
+    it("shouldn't be repeated ", function () {
+        var num = Number.create();
+        var a = parseInt(num / 1000);
+        var b = parseInt((num - a) / 100);
+        var c = parseInt((num - a - b) / 10);
+        var d = parseInt(num - a - b - c);
+
+        expect(a != b && a != c && a != d && b != c && b != d && c != d).toBe(true);
+    });
+
+    it("should be randomly generated", function () {
+        var nums = [];
+        for (var i = 0; i < 1000; i += 1) {
+            var num = Number.create();
+            nums.push(num);
+        }
+        var repeat = _.filter(nums, function (num) {
+            return num == nums[0];
+        });
+        var probability = repeat.length / 1000;
+
+        expect(probability >= 0.001 && probability < 0.007).toBe(true);
+    });
+
+});
---
+++ src/GenerateFourDigit.js
@@ -0,0 +1,18 @@
+function Number() {
+}
+
+Number.create = function () {
+    var a = parseInt(Math.random() * 9 + 1);
+    var b = parseInt(Math.random()*10);
+    var c = parseInt(Math.random()*10);
+    var d = parseInt(Math.random()*10);
+    var no_repeat = Number.is_not_repeat(a, b, c, d);
+    if (no_repeat) {
+        return a * 1000 + b * 100 + c * 10 + d;
+    }
+    return Number.create();
+}
+
+Number.is_not_repeat = function (a, b, c, d) {
+    return a != b && a != c && a != d && b != c && b != d && c != d;
+}
