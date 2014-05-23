5c2a8f22ec44905f84e4d15fd8f47d37804075b2
zhangyufei <momosuyu@gmail.com>
2014-05-23 10:59:14 +0800

    compare two numbers whether same

--- spec/two_num_is_?A?B_spec.js
+++ spec/two_num_is_?A?B_spec.js
@@ -6,7 +6,7 @@ describe("SimilarityIsWhatType", function () {
     it("should be compare 1234 and 5678 to 0A0B ", function () {
         var Type = SimilarityIsWhatType.is_what_type(1234, 5678);
 
-        expect(Type).toBe("0A4B");
+        expect(Type).toBe("0A0B");
     });
 
     it("should be compare 1234 and 1234 to 4A0B ", function () {
@@ -16,13 +16,13 @@ describe("SimilarityIsWhatType", function () {
     });
 
     it("should be compare 1234 and 4321 to 0A4B ", function () {
-        var Type = SimilarityIsWhatType.is_what_type(1234, 5678);
+        var Type = SimilarityIsWhatType.is_what_type(1234, 4321);
 
         expect(Type).toBe("0A4B");
     });
 
     it("should be compare 1234 and 1243 to 2A2B ", function () {
-        var Type = SimilarityIsWhatType.is_what_type(1234, 5678);
+        var Type = SimilarityIsWhatType.is_what_type(1234, 1243);
 
         expect(Type).toBe("2A2B");
     });
--- src/TwoNumIs?A?B.js
+++ src/TwoNumIs?A?B.js
@@ -5,6 +5,21 @@ function SimilarityIsWhatType() {
 SimilarityIsWhatType.is_what_type = function (num1, num2) {
     var four_num1 = SimilarityIsWhatType.get_a_single_num(num1);
     var four_num2 = SimilarityIsWhatType.get_a_single_num(num2);
+    var a=0;
+    var b=0;
+    for(var i=0;i<4;i+=1){
+       for(var j=0;j<4;j+=1){
+           if(four_num1[i] == four_num2[j] && i==j){
+                a+=1;
+
+           }
+           if(four_num1[i]==four_num2[j]&&i!=j){
+                b+=1;
+           }
+       }
+    }
+    return a+'A'+b+'B';
+
 }
 
 SimilarityIsWhatType.get_a_single_num = function (num) {
@@ -12,7 +27,8 @@ SimilarityIsWhatType.get_a_single_num = function (num) {
     var b = parseInt((num - a * 1000) / 100);
     var c = parseInt((num - a * 1000 - b * 100) / 10);
     var d = num % 10;
-    var four_number = {1: a, 2: b, 3: c, 4: d};
-
+    var four_number = [];
+    four_number.push(a,b,c,d);
     return four_number;
 }
+
